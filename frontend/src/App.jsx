de9a036029ed1b90fbc02f21350f00c2fcf350b8
Oussama Jabbari <oussama.jabbari.ext@kcitylabs.fr>
2023-03-14 16:02:56 +0100

    Home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,30 @@
-import './App.css'
+import './App.scss'
+import groupSelfie from './assets/group_selfie.png'
 
 function App() {
+  function openLogin() {
+    console.log('test');
+  }
+
   return (
     <div className="App">
+      <header>
+        <span className='icon'>
+          Connect<span className="goldy">Friends</span>
+        </span>
+        <span className='connection'>
+          <span className='login'>Login</span>
+          <span>Register</span>
+        </span>
+      </header>
+      <main>
+        <div className='slogan-and-join'>
+          <div className='slogan'>Share <span className='goldy'>good</span> Vibes</div>
+          <div className='slogan-text'>Friends are the sugar of your life, take care of them...</div>
+          <button onClick={openLogin}>Join and <span className='goldy'>Enjoy</span> !</button>
+        </div>
+        <img src={groupSelfie} className="selfie" alt="Group selfie" />
+      </main>
     </div>
   )
 }
